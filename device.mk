982d3d901aeeaed60608857be83639004b5deed6
fire855 <thefire855@gmail.com>
2017-10-08 13:06:34 +0200

    device: compile new fingerprint HAL

--- device.mk
+++ device.mk
@@ -51,7 +51,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    fingerprintd
+    android.hardware.biometrics.fingerprint@2.1-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
